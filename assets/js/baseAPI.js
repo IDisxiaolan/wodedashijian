53d76d03a00bfcceeedbca861054e2bbb8451085
xiaolanMac <827920850@qq.com>
2021-01-30 18:20:46 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,1 +1,18 @@
 axios.defaults.baseURL = 'http://ajax.frontend.itheima.net';
+$(() => {
+    $.ajaxPrefilter(function(options) {
+        if (options.url.indexOf('/my/') !== -1) {
+            options.headers = {
+                Authorization: localStorage.getItem('token') || ''
+            }
+        }
+        //不论请求成功失败都执行
+        options.complete = function(res) {
+            if (res.responseJSON.message == "身份认证失败！") {
+                //清空token;返回登陆页
+                localStorage.removeItem('token')
+                location.href = '/login.html'
+            }
+        }
+    })
+})
---
+++ assets/js/index.js
@@ -0,0 +1,39 @@
+$(() => {
+    const { form, layer } = layui
+
+    function getuserinfo() {
+        $.ajax({
+            method: 'get',
+            url: 'http://ajax.frontend.itheima.net/my/userinfo',
+
+            success: function(res) {
+                if (res.message !== "获取用户基本信息成功！") return layer.msg('获取用户信息失败')
+                layer.msg('获取用户信息成功')
+                console.log(res);
+
+                readerAvatar(res.data)
+            }
+        })
+    }
+    getuserinfo()
+
+    //渲染用户头像
+    function readerAvatar(user) {
+        var name = user.nickname || user.username
+        $('.nm').text(' ' + name)
+        if (user.user_pic == null) {
+            $('.layui-nav-img').hide()
+            var text = user.username[0].toUpperCase()
+            $('.text-avatar').html(text)
+        } else {
+            $('.text-avatar').hide()
+
+            $('.layui-nav-img').attr('url', user.user_pic).show()
+        }
+    }
+
+    $('.tuichu').click(function() {
+        localStorage.removeItem('token')
+        location.href = '/login.html'
+    })
+})
